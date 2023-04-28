b6e085848f265282ccc6ef0b1bf44ff2e42bb075
ronitshinde <rony.shinde753@gmail.com>
2023-04-28 19:23:57 +0530

    [Ronit] Add : UC1-Code to study Classes and Objects

---
+++ OopsConceptExample/OopsConceptExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConceptExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle example = new Vehicle();  //Object : "example" is the object used to call the type members of the class.
+            //Call First Method using "example" object.
+            example.Drive("Buggati", "White");
+            example.Drive("Ferrari", "Black");
+            //Second Method using object (Parameterized)
+            example.Brake("Disc Brake",007);
+            example.Brake("Drum Brake",0077);
+            //Third Method (non-parameterized with return type) using object
+            Console.WriteLine($"Gears : {example.GearShift()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OopsConceptExample/OopsConceptExample/Vehicle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConceptExample
+{
+    public class Vehicle   //Class : works as the blue print to create objects.
+    {
+        public string color;   //Variable : It works as the type member/attribute of the class.
+        public string brakeType; //All this variables are the Class level scope variables.
+        public string model;
+        public int modelNumber;
+        public void Drive(string carModel, string color)   //Method : works as the type member of the class.It is parameterized method.
+        {
+            model = carModel;
+            this.color = color;
+            Console.WriteLine($"This is the Drive method for a vehicle : {carModel} with {color} color");
+        }
+        internal void Brake(string brakeVarients, int modelNumber)
+        {
+            brakeType = brakeVarients;
+            this.modelNumber = modelNumber;
+            Console.WriteLine($"This {model} with model number {modelNumber} holds the {brakeType} variant");
+        }
+        public int GearShift()
+        {
+            int gearTransmission = 7;
+            Console.WriteLine($"Lamborghini has {gearTransmission} gear transmission");
+            return gearTransmission;
+        }
+    }
+}
