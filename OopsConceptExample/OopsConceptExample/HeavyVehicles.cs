3012e5259e144b682ac3740b34b58d5b713aa9b6
ronitshinde <rony.shinde753@gmail.com>
2023-04-28 20:33:28 +0530

    [Ronit] Add : UC2-Code to study Inheritance concept with example

---
+++ OopsConceptExample/OopsConceptExample/HeavyVehicles.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConceptExample
+{
+    //This is Inheritance : using properties of parent class/base class to the child class.
+    public class HeavyVehicles //This is parent/base class.
+    {
+        public string modelName;
+        public int modelNum;
+        public int wheels;
+        public string intensity;
+        public string enginePosition = "Front";
+        public int fuelCapacity = 100;
+        public void CarbonEmissions(string modelName, int modelNum, int wheels, string intensity)
+        {
+            this.modelName = modelName;
+            this.modelNum = modelNum;
+            this.wheels = wheels;
+            Console.WriteLine($"{modelName} heavy vehicle with model number : {modelNum} and {wheels} wheels has the {intensity} carbon emissions");
+        }
+    }
+    public class TransportRoutes : HeavyVehicles   //TransportRoutes is derived class and inherited properties from HeavyVehicles class.
+    {
+        string distance;
+        public void Transport(string distance)
+        {
+           this.distance = distance;
+            Console.WriteLine($"{enginePosition} engine position trucks have {distance} routes");
+            Console.WriteLine($"Trucks with fuel capacity more than {fuelCapacity} liters are good for {distance} routes");
+            Console.WriteLine("Engine Position variable inherited in TransportRoutes class : "+ enginePosition);
+            Console.WriteLine("Same for fuelCapacity : "+ fuelCapacity);
+        }
+    }
+}
--- OopsConceptExample/OopsConceptExample/Program.cs
+++ OopsConceptExample/OopsConceptExample/Program.cs
@@ -10,16 +10,36 @@ namespace OopsConceptExample
     {
         static void Main(string[] args)
         {
-            VehicleClass example = new VehicleClass();  //Object : "example" is the object used to call the type members of the class.
-            //Call First Method using "example" object.
-            example.Drive("Buggati", "White");
-            example.Drive("Ferrari", "Black");
-            //Second Method using object (Parameterized)
-            example.Brake("Disc Brake",007);
-            example.Brake("Drum Brake",0077);
-            //Third Method (non-parameterized with return type) using object
-            Console.WriteLine($"Gears : {example.GearShift()}");
-            Console.ReadKey();
+            Console.WriteLine("Select the concept");
+            Console.WriteLine("1.Object And Class\n2.Inheritance");
+            Console.Write("Enter the number : ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            switch (count)
+            {
+                case 1:
+                    VehicleClass example = new VehicleClass();  //Object : "example" is the object used to call the type members of the class.
+                                                                //Call First Method using "example" object.
+                    example.Drive("Buggati", "White");
+                    example.Drive("Ferrari", "Black");
+                    //Second Method using object (Parameterized)
+                    example.Brake("Disc Brake", 007);
+                    example.Brake("Drum Brake", 0077);
+                    //Third Method (non-parameterized with return type) using object
+                    Console.WriteLine($"Gears : {example.GearShift()}");
+                    Console.ReadKey();
+                    return;
+                case 2:
+                    //Calling type members of base class using child class object.
+                    TransportRoutes routes = new TransportRoutes();
+                    routes.CarbonEmissions("Ashok Leyland", 7451, 14, "HIGHEST");
+                    routes.CarbonEmissions("Eicher", 6142, 12, "LOWEST");
+                    routes.Transport("LONGER");
+                    routes.Transport("SHORTER");
+                    Console.WriteLine();
+                    Console.ReadKey();
+                    return;
+            }
         }
     }
 }
